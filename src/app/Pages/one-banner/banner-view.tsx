bb60a8944f72dfa5e46b0ba1d2ed2c9bad52759e
Eriveltonssa <eriveltonmagalhaesdossantos@gmail.com>
2025-04-07 01:27:42 -0300

    feat: add cards de produtos

---
+++ src/app/Pages/one-banner/banner-view.tsx
@@ -0,0 +1,20 @@
+
+// ;IMPORTAÇÃO CSS 
+import './banner-view.css'
+
+function BannerViewONe() {
+    return ( 
+
+        <>
+        
+         <section className='container_banner_one'>
+
+
+         </section>
+        
+        
+        </>
+     );
+}
+
+export default BannerViewONe;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
-
-
 // AQUI FICA AS IMPORAÇÕES DOS COMPONENTS
+import CardProducts from "./Components/cardsProducts/cardProdutucs";
 import Header from "./Components/header/header";
 import HomePage from "./Pages/home/home";
-
+import BannerViewONe from "./Pages/one-banner/banner-view";
 
 export default function Home() {
   return (
     <>
-    {/* Header */}
-     <Header></Header>    
-     {/* Home */}
-     <HomePage></HomePage>
-
-     </>
+      {/* Header */}
+      <Header></Header>
+      {/* Home */}
+      <HomePage></HomePage>
+      {/* carrosel de produtos */}
+      <CardProducts></CardProducts>
+      {/* BannerViewONe */}
+      <BannerViewONe></BannerViewONe>
+    </>
   );
 }
